fbf62bc9a58e89d7808d087198ea734e8bbcd6f9
Ato Toffah <atotoffah@gmail.com>
2024-04-11 22:13:06 +0000

    Task 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -25,30 +25,6 @@ def filter_datum(
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
-class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Formats an incoming log message"""
-        message = super(RedactingFormatter, self).format(record)
-        res = filter_datum(
-            self.fields,
-            RedactingFormatter.REDACTION,
-            message,
-            RedactingFormatter.SEPARATOR,
-        )
-
-        return res
-
-
 def get_logger() -> logging.Logger:
     """Creates a logger"""
 
@@ -76,3 +52,60 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=db_name,
     )
     return connection
+
+
+def main():
+    """Logs the information about user records in a table."""
+    columns = [
+        "name",
+        "email",
+        "phone",
+        "ssn",
+        "password",
+        "ip",
+        "last_login",
+        "user_agent",
+    ]
+    query = "SELECT {} FROM users;".format(",".join(columns))
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: "{}={}".format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = "{};".format("; ".join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats an incoming log message"""
+        message = super(RedactingFormatter, self).format(record)
+        res = filter_datum(
+            self.fields,
+            RedactingFormatter.REDACTION,
+            message,
+            RedactingFormatter.SEPARATOR,
+        )
+
+        return res
+
+
+if __name__ == "__main__":
+    main()
