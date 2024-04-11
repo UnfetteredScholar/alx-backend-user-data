b2c699e4eb33703b101542589243053dc2f997ae
Ato Toffah <atotoffah@gmail.com>
2024-04-11 20:54:54 +0000

    Task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Defines the filter_datum function"""
+from typing import List
+import re
+
+patterns = {
+    "extract": lambda x, y: r"(?P<field>{})=[^{}]*".format("|".join(x), y),
+    "replace": lambda x: r"\g<field>={}".format(x),
+}
+
+
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str,
+) -> str:
+    """Filters a log string."""
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
