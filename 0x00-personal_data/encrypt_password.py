4f5c38de96756aa6fd5fc92bc62e66c0b8c766b9
Ato Toffah <atotoffah@gmail.com>
2024-04-11 22:25:47 +0000

    Task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,9 @@ def hash_password(password: str) -> bytes:
     hash = bcrypt.hashpw(bytes, salt)
 
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a plaintext password matches a hash"""
+
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
