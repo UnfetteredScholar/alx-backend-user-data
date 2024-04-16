022ea55c71d1fecaeefcc0e4aec478c96f30ed73
Ato Toffah <atotoffah@gmail.com>
2024-04-16 17:48:54 +0000

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Defines the BasicAuth class"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -52,3 +54,21 @@ class BasicAuth(Auth):
         ):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar("User"):
+        """Gets a user object from storage that matches the credentials"""
+
+        if user_email is None or type(user_email) is not str:
+            return None
+
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        users = User.search({"email": user_email})
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
