32b0a3b98859184d830fb38af4dcf34ea39a8ae5
Ato Toffah <atotoffah@gmail.com>
2024-04-16 15:37:41 +0000

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,17 @@ class Auth:
     """The auth class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require_auth function"""
-        return False
+        """
+        Returns true if a path is not in
+        the excluded paths else false
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != "/":
+            path = path + "/"
+
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """authorization_header function"""
