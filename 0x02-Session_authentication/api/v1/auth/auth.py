66f0df880e967efed0330ff837f1e37c25c78f14
Ato Toffah <atotoffah@gmail.com>
2024-04-18 22:09:43 +0000

    Task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines the auth class"""
 import re
+import os
 from flask import request
 from typing import List, TypeVar
 
@@ -9,17 +10,16 @@ class Auth:
     """The auth class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
-        """
+        """Checks if a path requires authentication."""
         if path is not None and excluded_paths is not None:
             for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                pattern = ""
+                if exclusion_path[-1] == "*":
+                    pattern = "{}.*".format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == "/":
+                    pattern = "{}/*".format(exclusion_path[0:-1])
                 else:
-                    pattern = '{}/*'.format(exclusion_path)
+                    pattern = "{}/*".format(exclusion_path)
                 if re.match(pattern, path):
                     return False
         return True
@@ -34,3 +34,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar("User"):
         """Returns the current user"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns the cookie SESSION_NAME value from the request"""
+
+        if request is not None:
+            cookie_name = os.getenv("SESSION_NAME")
+            return request.cookies.get(cookie_name)
