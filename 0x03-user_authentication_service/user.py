650be290e73576cb96fced6aba572d6217f28df9
Ato Toffah <atotoffah@gmail.com>
2024-04-24 19:53:09 +0000

    Task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines the User model"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, VARCHAR
+
+Base = declarative_base()
+
+
+class User(Base):
+    """The User Model"""
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=True)
+    reset_token = Column(VARCHAR(250), nullable=True)
